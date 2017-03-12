40d8051a1fabd1d476ca70c717d371c5843a6809
jasonwu82 <hungwei@umich.edu>
2017-03-12 16:03:29 -0400

    relu6 wih 3 layer network

--- train_pal.py
+++ train_pal.py
@@ -101,11 +101,11 @@ def inference(images):
   #for i in range(len(settings.depth)-1):
   #  prev_in = conv_layer(prev_in,settings.depth[i],settings.depth[i+1],'conv'+ str(i),conv_layer_dict)
   conv1 = conv_layer(images,settings.layer_depth['conv1'],'conv1')
-  conv2 = conv_layer(conv1,settings.layer_depth['conv2'],'conv2')
-  conv3 = conv_layer(conv2,settings.layer_depth['conv3'],'conv3')
-  conv4 = conv_layer(conv3,settings.layer_depth['conv4'],'conv4')
-  conv5 = conv_layer(conv4,settings.layer_depth['conv5'],'conv5')
-  conv5 = debug_tensor(conv5)
+  #conv2 = conv_layer(conv1,settings.layer_depth['conv2'],'conv2')
+  #conv3 = conv_layer(conv2,settings.layer_depth['conv3'],'conv3')
+  conv4 = conv_layer(conv1,settings.layer_depth['conv4'],'conv4')
+  conv_last = conv_layer(conv4,settings.layer_depth['conv_last'],'conv_last')
+  conv_last = debug_tensor(conv_last)
   #conv5_shape = debug_tensor(tf.shape(conv5))
   #for k in conv_layer_dict:
   #  shape_dict[k] = tf.shape(conv_layer_dict[k])
@@ -119,14 +119,14 @@ def inference(images):
     #b = tf.zeros(shape=[settings.NUM_CLASSES])
     b = debug_tensor(b)
     #b = debug_tensor(b)
-    w = tf.get_variable("weight",shape=[5, 5, settings.NUM_CLASSES,settings.layer_depth['conv5'][1]] )
+    w = tf.get_variable("weight",shape=[5, 5, settings.NUM_CLASSES,settings.layer_depth['conv_last'][1]] )
     #w = tf.ones([5, 5, settings.NUM_CLASSES,settings.layer_depth['conv1'][1]])
     w = debug_tensor(w)
     #w = debug_tensor(w)
     out_shape = tf.pack([settings.BATCH_SIZE,tf.shape(images)[1],tf.shape(images)[2],settings.NUM_CLASSES])
     out_shape = debug_tensor(out_shape)
-    deconv = tf.nn.conv2d_transpose(conv5, 
-      w, output_shape=out_shape, strides=[1, 32, 32, 1], padding="SAME")
+    deconv = tf.nn.conv2d_transpose(conv_last, 
+      w, output_shape=out_shape, strides=[1, 8, 8, 1], padding="SAME")
     deconv32 = tf.nn.bias_add(deconv, b)
     deconv32 = debug_tensor(deconv32)
 
